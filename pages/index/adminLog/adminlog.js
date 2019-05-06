95f93b1ccb3afc04ead2df78dda093f82881e9a0
ZiyeChen <645858695@qq.com>
2019-05-06 09:10:13 +0800

    login-test

--- pages/index/adminLog/adminlog.js
+++ pages/index/adminLog/adminlog.js
@@ -17,14 +17,58 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  getCode:function(e){
     var that=this;
-    var a=wx.getStorageSync('code');
-    if(a){
-      that.setData({
-        code: a
-      })
-    }
+    var val = e.detail.value;
+    that.setData({
+      code: val
+    });
+    wx.setStorageSync('code', val);
+
+  },
+  login:function(e){
+    var code=this.data.code;
+    var url = getApp().data.root +'/main/user/logInAdminAccount';
+    wx.request({
+      url: url,
+      method: "POST",
+      data: {
+        code: code
+      },
+      success: function (e) {
+        console.log(e.data.status);
+        if (e.data.status == true) {
+          wx.showToast({
+            title: '登录成功',
+            icon: 'success',
+            duration: 1500,
+            success: function () {
+              setTimeout(function () {
+                // wx.redirectTo({
+                //   url: '/pages/index/logcode/code?code=' + code,
+                // });
+              }, 1500)
+            }
+          });
+        } else {
+          wx.hideToast();
+          wx.showModal({
+            content: '登录失败了狗东西',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+              }
+            }
+          });
+        }
+      }
+    })
+
+
+
+
+  },
+  onLoad: function (options) {
 
   },
 
@@ -39,6 +83,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    var that = this;
+    var a = wx.getStorageSync('code');
+    if (a) {
+      that.setData({
+        code: a
+      })
+    }
 
   },
 
